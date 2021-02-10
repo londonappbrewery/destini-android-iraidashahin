8d4716cb46b60d18e8864fb9616dfae47ffc4798
ilayda <iruyadachan@gmail.com>
2021-02-10 21:55:56 +0300

    assignment finished

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -1,11 +1,19 @@
 package com.londonappbrewery.destini;
 
+import android.support.annotation.StringRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
+    Button mTopButton, mBotButton;
+    TextView mQuestion;
+    int mStoryIndex = 1;
+    
 
 
     @Override
@@ -16,15 +24,62 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
 
+        mTopButton = findViewById(R.id.buttonTop);
+        mBotButton = findViewById(R.id.buttonBottom);
+        mQuestion = findViewById(R.id.storyTextView);
+
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
 
+        mTopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mStoryIndex == 1 || mStoryIndex == 2) {
+                    mQuestion.setText(R.string.T3_Story);
+                    mTopButton.setText(R.string.T3_Ans1);
+                    mBotButton.setText(R.string.T3_Ans2);
+                    if(mStoryIndex == 1) {
+                        mStoryIndex = 3;
+                    }
+                }
+                else if(mStoryIndex == 3) {
+                    mQuestion.setText(R.string.T6_End);
+                    mTopButton.setVisibility(View.GONE);
+                    mBotButton.setVisibility(View.GONE);
+                }
+            }
+        });
 
 
 
         // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
 
+        mBotButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(mStoryIndex == 1){
+                    mQuestion.setText(R.string.T2_Story);
+                    mTopButton.setText(R.string.T2_Ans1);
+                    mTopButton.setText((R.string.T2_Ans2));
+                    mStoryIndex = 2;
+                }
+                else if (mStoryIndex == 2){
+                    mQuestion.setText(R.string.T4_End);
+                    mTopButton.setVisibility(View.GONE);
+                    mBotButton.setVisibility(View.GONE);
+                }
+                else if(mStoryIndex == 3){
+                    mQuestion.setText(R.string.T5_End);
+                    mTopButton.setVisibility(View.GONE);
+                    mBotButton.setVisibility(View.GONE);
+                }
+            }
+        });
+
+    }
+
+    private void update(){
 
     }
 }
